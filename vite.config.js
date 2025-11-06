6eb9e3a2abb85c26000d68a012640be45339fec5
zaykha <zaykha@gmail.com>
2025-11-06 18:16:20 +0900

    netlify config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MoeZayKha/',
+  // Use root base for Netlify (and any custom domain)
+  base: '/',
   plugins: [react(), splitVendorChunkPlugin()],
   build: {
     chunkSizeWarningLimit: 1600,
